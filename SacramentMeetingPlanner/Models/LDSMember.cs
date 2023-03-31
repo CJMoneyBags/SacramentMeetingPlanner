d5a0f9e928876666834fb5f0344da75ece8f1380
Oleksii Vasiuk <oleksiivasiuk@byui.edu>
2023-03-30 21:15:43 -0600

    adds initial CRUD elements and migrations to the Database.

--- SacramentMeetingPlanner/Models/LDSMember.cs
+++ SacramentMeetingPlanner/Models/LDSMember.cs
@@ -4,10 +4,22 @@ namespace SacramentMeetingPlanner.Models
 {
     public class LDSMember
     {
+        public int Id { get; set; }
         public int RecordNumber { get; private set; }
         public string FullName { get; set; }
         public DateTime BirthDay { get; set; }
         // just added for the fun of it :D
         public bool MusicalSkills { get; set; }
+
+        public LDSMember(int id, int recordNumber, string fullName, DateTime birthDay, bool musicalSkills)
+        {
+            Id = id;
+            RecordNumber = recordNumber;
+            FullName = fullName;
+            BirthDay = birthDay;
+            MusicalSkills = musicalSkills;
+        }
+
+        public LDSMember() { }
     }
 }
--- SacramentMeetingPlanner/Models/SacramentMeeting.cs
+++ SacramentMeetingPlanner/Models/SacramentMeeting.cs
@@ -6,15 +6,34 @@ namespace SacramentMeetingPlanner.Models
 {
     public class SacramentMeeting
     {
+        public int Id { get; set; }
         public DateTime Date { get; set; }
         public string ConductingLeaderName { get; set; }
-        public List<LDSMember> Speakers { get; set; }
+        public List<LDSMember> Speakers = new List<LDSMember>();
         public int OpeningHymn { get; set; }
         public int SacramentHymn { get; set; }
-        public int? IntermediateHymn { get; set; }
-        public string? MusicalNumber { get; set; }
+        public string? IntermediateHymnOrMusicalNumber { get; set; }
         public int ClosingHymn { get; set; }
         public string OpeningPrayerPerson { get; set; }
         public string ClosingPrayerPerson { get; set; }
+
+        public SacramentMeeting(int id, DateTime date,
+            string conductingLeaderName, int openingHymn,
+            int sacramentHymn, string? intermediateHymnOrMusicNumber,
+            int closingHymn, string openingPrayerPerson,
+            string closingPrayerPerson)
+        {
+            Id = id;
+            Date = date;
+            ConductingLeaderName = conductingLeaderName;
+            OpeningHymn = openingHymn;
+            SacramentHymn = sacramentHymn;
+            IntermediateHymnOrMusicalNumber = intermediateHymnOrMusicNumber;
+            ClosingHymn = closingHymn;
+            OpeningPrayerPerson = openingPrayerPerson;
+            ClosingPrayerPerson = closingPrayerPerson;
+        }
+
+        public SacramentMeeting() { }
     }
 }
