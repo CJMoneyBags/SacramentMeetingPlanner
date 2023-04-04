0d0d6fef0c54b3b3597bf42f9fda7eb79be4fa01
Oleksii Vasiuk <oleksiivasiuk@byui.edu>
2023-04-03 20:21:38 -0600

    adds validation to the SacramentMeeting.

--- SacramentMeetingPlanner/Models/SacramentMeeting.cs
+++ SacramentMeetingPlanner/Models/SacramentMeeting.cs
@@ -7,25 +7,40 @@ namespace SacramentMeetingPlanner.Models
     {
         public int Id { get; set; }
         public DateTime Date { get; set; }
+        
         [Display(Name = "Conducting Leader")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(30)]
         public string ConductingLeaderName { get; set; }
+        
         [Display(Name = "Opening Hymn")]
+        [Range(1,341)]
         public int OpeningHymn { get; set; }
+        
         [Display(Name = "Sacrament Hymn")]
-
+        [Range(1, 341)]
         public int SacramentHymn { get; set; }
+        
         [Display(Name = "Intermediate Hymn Or Musical Number")]
-
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(30)]
         public string? IntermediateHymnOrMusicalNumber { get; set; }
+
         [Display(Name = "Closing Hymn")]
-
+        [Range(1, 341)]
         public int ClosingHymn { get; set; }
+
         [Display(Name = "Opening Prayer")]
-
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(30)]
         public string OpeningPrayerPerson { get; set; }
+        
         [Display(Name = "Closing Prayer")]
-
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(30)]
         public string ClosingPrayerPerson { get; set; }
+
+        [Display(Name = "Talk Subjects")]
         public List<string>? SpeakerSubjects { get; set; }
     }
 }
